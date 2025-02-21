31eca75e39e871d210cc25397e6f9fd7f91aa621
Jorge Roberto <jorgercj@dcomp.ufs.br>
2025-02-20 23:07:00 -0300

    Remove useless comments

--- CleanCodeApp/Modules/Features/Ceu/ContactsUs/CeuContactUsStrings.swift
+++ CleanCodeApp/Modules/Features/Ceu/ContactsUs/CeuContactUsStrings.swift
@@ -6,10 +6,6 @@
 //
 
 enum CeuContactUsStrings: String {
-//    // MARK: View Model Strings
-//    case verifyEmailErrorMessage = "Verifique o e-mail informado."
-//    case somethingWentWrongErrorMessage = "Algo de errado aconteceu. Tente novamente mais tarde."
-//
     // MARK: View Strings
     case writeYourMessage = "Escreva sua mensagem aqui"
     case chooseYourContactChannel = "Escolha o canal para contato"
@@ -19,16 +15,12 @@ enum CeuContactUsStrings: String {
 
     // MARK: View Controller Strings
     case initFatalErrorMessage = "init(coder:) has not been implemented."
-//
+
     // MARK: Coordinator Strings
     case ops = "Ops..."
     case anErrorOccurred = "Ocorreu algum erro"
     case success = "Sucesso"
     case yourMessageHasBeenSend = "Sua mensagem foi enviada"
-//
-//    // MARK: CeuGlobalsProtocol Strings
-//    case noConnection = "Sem Conexão"
-//    case connectToInternetErroMessage = "Conecte-se à internet para tentar novamente"
 
     func localized() -> String {
         return self.rawValue
